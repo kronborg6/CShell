0c67f18d725941f123ab78985e0b769e30bdd190
Kronborg <mkronborg7@gmail.com>
2025-04-09 22:25:24 +0200

    add a function to hold commands

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "helpers.h"
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
+#include <string.h>
+int mainOLD() {
 	char loo[] = "Monday Tursday Wensday";
 	char **tokens;
 	char *str = NULL;
@@ -25,5 +28,35 @@ int main() {
 	}
 	int foo = 1;
 
+	return 0;
+}
+
+bool run = true;
+
+void Commands(char *command, int count, ...) {
+	va_list args;
+	va_start(args, count);
+
+	command[strcspn(command, "\n")] = 0;
+
+	if (strcmp(command, "exit") == 0) {
+		printf("sorry to se you go\n");
+		run = false;
+	}
+	if (strcmp(command, "echo") == 0) {
+		printf("hello\n");
+	}
+}
+int main() {
+	char *str = NULL;
+	size_t len = 0;
+	ssize_t read;
+	while (run) {
+		printf("> ");
+		read = getline(&str, &len, stdin);
+		if (read != -1) {
+			Commands(str, 0);
+		}
+	}
 	return 0;
 }
