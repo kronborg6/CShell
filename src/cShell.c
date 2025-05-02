d7f18bc10f1be2de0458d4e3107091bd4a2e21af
Kronborg <mkronborg7@gmail.com>
2025-05-02 22:27:00 +0200

    start to replace home_path aka /home/kronborg with ~

--- src/cShell.c
+++ src/cShell.c
@@ -1,4 +1,6 @@
 #include "cShell.h"
+#include "helpers.h"
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +18,40 @@ int init_system_path() {
 
 	if ((buf = (char *)malloc((size_t)system_path_max_len)) != NULL) {
 		system_path = getcwd(buf, (size_t)system_path_max_len);
+		char **sliced;
+		char **home_sliced;
+		int sliced_path_len = 0;
+		int home_sliced_len = 0;
+		char *home = getenv("HOME");
+		if (home != NULL) {
+			sliced = splitString(system_path, '/');
+			home_sliced = splitString(home, '/');
+
+			while (sliced[sliced_path_len] != NULL) {
+				sliced_path_len++;
+			}
+			while (home_sliced[home_sliced_len] != NULL) {
+				home_sliced_len++;
+			}
+			if (sliced_path_len >= 2 && home_sliced_len >= 2) {
+				if (strcmp(sliced[0], home_sliced[0]) == 0 && strcmp(sliced[1], home_sliced[1]) == 0) {
+					/* system_path = "~"; */
+				}
+			}
+
+			char *out = malloc(system_path_max_len);
+
+			char *p = out;
+			*p++ = '~';
+			for (size_t i = 2; i < sliced_path_len; ++i) {
+				*p++ = '/';
+				size_t sliced_path_len = strlen(sliced[i]);
+				memcpy(p, sliced[i], sliced_path_len);
+				p += sliced_path_len;
+			}
+			*p = '\0';
+			system_path = out;
+		}
 		/* printf("%s\n", system_path); */
 		return 0;
 	} else {
--- temp/cd.c
+++ temp/cd.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 int main() {
-	long size;
-	char *buf;
-	char *ptr;
-	size = pathconf(".", _PC_PATH_MAX);
+	/* long size; */
+	/* char *buf; */
+	/* char *ptr; */
+	/* size = pathconf(".", _PC_PATH_MAX); */
+	char *foo = getenv("HOME");
+	printf("%s\n", foo);
 
-	if ((buf = (char *)malloc((size_t)size)) != NULL) {
-		ptr = getcwd(buf, (size_t)size);
-		printf("%s\n", ptr);
-	}
+	/* if ((buf = (char *)malloc((size_t)size)) != NULL) { */
+	/* 	ptr = getcwd(buf, (size_t)size); */
+	/* 	printf("%s\n", ptr); */
+	/* } */
 
 	return 0;
 }
